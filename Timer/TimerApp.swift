18e9b9b85214d9bd0b5bf9dd8129134ae733f953
Shiyan Chen <ch592127418@gmail.com>
2023-02-05 12:03:42 -0800

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by 陈世彦 on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
